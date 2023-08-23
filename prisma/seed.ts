a005c6ff3aa9a237906e219b85355ea7349e0818
coji <coji@techtalk.jp>
2023-08-23 21:25:24 +0900

    remove unused file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,6 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
+// const prisma = new PrismaClient()
 
 async function seed() {
-  await prisma.user.upsert({
-    where: { email: 'test@example.com' },
-    create: { displayName: 'John Doe', email: 'test@example.com' },
-    update: { displayName: 'John Doe' },
-  })
   console.log(`Database has been seeded. 🌱`)
 }
 
